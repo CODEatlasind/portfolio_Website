fb45da0992b8266011f36ae479fc595c62a7a99d
Shashank <shashankshekharsn042@gmail.com>
2026-08-29 17:09:34 +0530

    Enabled cors

--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -5,7 +5,9 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
+
 app.use((req, res, next) => {console.log(`${req.method} ${req.url}`); 
 next();
 });
