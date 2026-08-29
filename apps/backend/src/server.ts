fb374af9912161589a939fbe8dbe072316d085a1
Shashank <shashankshekharsn042@gmail.com>
2026-08-29 17:19:02 +0530

    Logger and port placement interchanged

--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -3,14 +3,14 @@ import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config();
 const app = express();
-const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 
-app.use((req, res, next) => {console.log(`${req.method} ${req.url}`); 
+app.use((req, _res, next) => {console.log(`${req.method} ${req.url}`); 
 next();
 });
+const port = Number(process.env.PORT) || 5000; // Accepts port number as string so conversion is needed to number type
 
 app.get("/api/health", (_req, res) => {  // _req is used to indicate that the request parameter is intentionally unused
     res.json({ 
