e2e27bf969a239eea160f8eca33c29c0891de76e
Shashank <shashankshekharsn042@gmail.com>
2026-09-07 01:21:25 +0530

    Added validation error handling at middleware

--- apps/backend/src/middleware/error.middleware.ts
+++ apps/backend/src/middleware/error.middleware.ts
@@ -1,5 +1,6 @@
 // A middleware that handles errors in the application. It logs the error and sends a generic error response to the client.
 import { type Request, type Response, type NextFunction } from "express";
+import mongoose from "mongoose";
 export const errorMiddleware = (
   err: unknown,
   _req: Request,
@@ -7,5 +8,12 @@ export const errorMiddleware = (
   _next: NextFunction,
 ) => {
   console.error(err);
+  if (err instanceof mongoose.Error.ValidationError) {
+    res.status(400).json({
+      message: "Validation error",
+      error: err.message,
+    });
+    return;
+  }
   res.status(500).json({ message: "An unexpected error occurred" });
 };
